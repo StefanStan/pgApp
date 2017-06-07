065838e305d7b1094b4d5fbd9c9c3f8a56180406
Stefan Stan <stefan.stan93@yahoo.ro>
2017-06-07 13:50:07 +0300

    CORS fix

--- src/main/java/pgapp/Application.java
+++ src/main/java/pgapp/Application.java
@@ -18,7 +18,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("*");
+                registry.addMapping("/admin/**").allowedOrigins("*");
             }
         };
     }
